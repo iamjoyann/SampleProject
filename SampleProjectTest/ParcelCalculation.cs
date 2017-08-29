3e7576a72c198085f181001c0a2bf72a7ab1b090
iamjoyann <31440964+iamjoyann@users.noreply.github.com>
2017-08-29 22:50:24 +0800

    Add files via upload

---
+++ SampleProjectTest/ParcelCalculation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SampleProject.Controllers;
+
+namespace SampleProjectTest
+{
+    [TestClass]
+    public class ParcelCalculation
+    {
+        [TestMethod]
+        public void GetRule()
+        {
+            var controller = new ParcelCalculationController();
+            var getrule = controller.GetRule(10, 10);
+            Assert.AreEqual("Small", getrule);
+        }
+
+        [TestMethod]
+        public void GetCost()
+        {
+            var controller = new ParcelCalculationController();
+            var getcost = controller.GetCost(10, 10, "Small");
+            Assert.AreEqual("40", getcost);
+        }
+    }
+}
